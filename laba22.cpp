f9f8e7cd76a3754d0955f0a639b575407831f331
Rifia <katerain45@gmail.com>
2018-02-27 01:58:13 +0300

    string arr(int r)

--- laba22.cpp
+++ laba22.cpp
@@ -2,31 +2,39 @@
 #include <cstring>
 using namespace std;
 void getline( char* buffer, streamsize num);
+
+string arr(int r){
+    string word;
+    switch(r){
+	case 48: word = "zero"; break;
+	case 49: word = "one"; break;
+	case 50: word = "two"; break;
+	case 51: word = "three"; break;
+	case 52: word = "four"; break;
+	case 53: word = "five"; break;
+	case 54: word = "six"; break;
+	case 55: word = "seven"; break;
+	case 56: word = "eight"; break;
+	case 57: word = "nine"; break;
+	}
+return word;
+}
+
 int main()
 {
-	string a; int z = -1; int r; string word;
+	string a, word; int z = -1, r; 
 	getline(cin, a);
 	char* b = new char[20];
 	int w = a.length();
 	for(int i = 0; i<w; i++){
 	    r = int (a[i]);
 	    if(r>=48&&r<=57){
-	        switch(r){
-	    case 48: word = "zero"; break;
-		case 49: word = "one"; break;
-		case 50: word = "two"; break;
-		case 51: word = "three"; break;
-		case 52: word = "four"; break;
-		case 53: word = "five"; break;
-		case 54: word = "six"; break;
-		case 55: word = "seven"; break;
-		case 56: word = "eight"; break;
-		case 57: word = "nine"; break;
-	            }
-	int y = word.length(); 
+		word = arr(r);
+	        int y = word.length(); 
 	        for (int i = 0; i < y; i++) {
-			    z++;
-			    *(b+z) = word[i];}
+			z++;
+			*(b+z) = word[i];
+		}
 	   }
 	    else {z++; *(b+z)=a[i];}
 	}
