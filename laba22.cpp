dd41527ade340626066d4d3d40859aa87a3ac29f
Rifia <katerain45@gmail.com>
2018-02-27 01:50:38 +0300

    Update laba22.cpp

--- laba22.cpp
+++ laba22.cpp
@@ -1,37 +1,36 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 using namespace std;
-
-string rpl (char k){
-    string word;
-    switch (k){
-        case '0': word = "zero"; break;
-        case '1': word = "one"; break;
-        case '2': word = "two"; break;
-        case '3': word = "three"; break;
-        case '4': word = "four"; break;
-        case '5': word = "five"; break;
-        case '6': word = "six"; break;
-        case '7': word = "seven"; break;
-        case '8': word = "eight"; break;
-        case '9': word = "nine"; break;
-        default: word = k;
-    }
-    return word;
-}
-
+void getline( char* buffer, streamsize num);
 int main()
 {
-    string str;
-    string str1;
-    char w;
-    getline(cin, str);
-    for (int i = 0; i < str.length(); i++){
-        char w = *(str+i);
-        str1 += rpl(w);
-    }
-    
-    
-    return 0;
+	string a; int z = -1; int r; string word;
+	getline(cin, a);
+	char* b = new char[20];
+	int w = a.length();
+	for(int i = 0; i<w; i++){
+	    r = int (a[i]);
+	    if(r>=48&&r<=57){
+	        switch(r){
+	    case 48: word = "zero"; break;
+		case 49: word = "one"; break;
+		case 50: word = "two"; break;
+		case 51: word = "three"; break;
+		case 52: word = "four"; break;
+		case 53: word = "five"; break;
+		case 54: word = "six"; break;
+		case 55: word = "seven"; break;
+		case 56: word = "eight"; break;
+		case 57: word = "nine"; break;
+	            }
+	int y = word.length(); 
+	        for (int i = 0; i < y; i++) {
+			    z++;
+			    *(b+z) = word[i];}
+	   }
+	    else {z++; *(b+z)=a[i];}
+	}
+	for(int j=0;j<=z;j++)cout<<*(b+j);
+	
+	return 0;
 }
-
