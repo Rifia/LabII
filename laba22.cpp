81793fbcbc7ed7b6550f8bd1b40b993dd0a6f2f9
Rifia <katerain45@gmail.com>
2018-02-27 08:28:30 +0300

    Update laba22.cpp

--- laba22.cpp
+++ laba22.cpp
@@ -1,45 +1,44 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-void getline( char* buffer, streamsize num);
 
-string arr(int r){
+string arr(char r){
     string word;
     switch(r){
-	case 48: word = "zero"; break;
-	case 49: word = "one"; break;
-	case 50: word = "two"; break;
-	case 51: word = "three"; break;
-	case 52: word = "four"; break;
-	case 53: word = "five"; break;
-	case 54: word = "six"; break;
-	case 55: word = "seven"; break;
-	case 56: word = "eight"; break;
-	case 57: word = "nine"; break;
-	}
-return word;
+        case '0': word = "zero"; break;
+        case '1': word = "one"; break;
+        case '2': word = "two"; break;
+        case '3': word = "three"; break;
+        case '4': word = "four"; break;
+        case '5': word = "five"; break;
+        case '6': word = "six"; break;
+        case '7': word = "seven"; break;
+        case '8': word = "eight"; break;
+        case '9': word = "nine"; break;
+    }
+    return word;
 }
 
 int main()
 {
-	string a, word; int z = -1, r; 
-	getline(cin, a);
-	int w = a.length();
-	int size = w*5;
-	char* b = new char[size];
-	for(int i = 0; i<w; i++){
-	    r = int (a[i]);
-	    if(r>=48&&r<=57){
-		word = arr(r);
-	        int y = word.length(); 
-	        for (int i = 0; i < y; i++) {
-			z++;
-			*(b+z) = word[i];
-		}
-	   }
-	    else {z++; *(b+z)=a[i];}
-	}
-	for(int j=0;j<=z;j++)cout<<*(b+j);
-	
-	return 0;
+    string a; int z = -1;
+    getline(cin, a);
+    unsigned long w = a.length();
+    char* b = new char[5*w];
+    for(int i = 0; i<w; i++){
+        if(a[i]>='0'&&a[i]<='9'){
+            string word = arr(a[i]);
+            unsigned long y = word.length();
+            for (int i = 0; i < y; i++) {
+                z++;
+                *(b+z) = word[i];
+            }
+        }
+        else {z++; *(b+z)=a[i];}
+    }
+    for(int j=0;j<=z;j++)cout<<*(b+j);
+    delete[]b;
+    cout << endl;
+    
+    return 0;
 }
