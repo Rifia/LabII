720c7c45c94f6a1887dce3809cb6a491b7c078cf
Rifia <katerain45@gmail.com>
2018-03-05 23:05:24 +0300

    Update laba23.cpp

--- laba23.cpp
+++ laba23.cpp
@@ -1,27 +1,41 @@
+// Fileio.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+
 #include <iostream>
-#include <cstring>
+#include <string>
 #include <fstream>
 using namespace std;
 
 int main() {
-    
-    ifstream file("input.txt");//open file
-    
-    string filename;
-    cin >> filename;
-    ofstream ofs(filename.c_str());
-    
-    char *str = new char [1024];//count strings
-    int i=0;
-    while (!file.eof())
-    {
-        file.getline(str, 1024, '\n');
-        i++;
-    }
-    delete[]str;
-    
-    for(int j = i; j<i; j++){
-       
-        }
-return 0;
+	ifstream in;
+	in.open("input.txt");
+
+	string str;
+	int count = 0;
+	int linenum = 0;
+
+	while (getline(in, str)) { linenum++; }
+	cout << linenum;
+
+	ofstream out;
+	out.open("output.txt");
+
+	while (getline(in, str)) {
+		int x = 0;
+		count++;
+		while (x < str.length()) {
+			if (str[x] <= '0' && str[x] >= '9'&& count!=(linenum || linenum-1)) break;
+			else x++;
+		}
+		if(x == str.length()-1) out << str;
+		cout << str;
+	}
+
+	in.close();
+	out.close();
+    return 0;
 }
+
