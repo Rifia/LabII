0b1fbca766e1bb13c23a6c4bd20c02392f38718e
Rifia <katerain45@gmail.com>
2018-03-06 09:35:46 +0300

    доделать фильтр строк

--- laba23.cpp
+++ laba23.cpp
@@ -1,41 +1,42 @@
-// Fileio.cpp: определяет точку входа для консольного приложения.
-//
-
-#include "stdafx.h"
-
-
 #include <iostream>
-#include <string>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main() {
-	ifstream in;
-	in.open("input.txt");
-
-	string str;
-	int count = 0;
-	int linenum = 0;
-
-	while (getline(in, str)) { linenum++; }
-	cout << linenum;
-
-	ofstream out;
-	out.open("output.txt");
-
-	while (getline(in, str)) {
-		int x = 0;
-		count++;
-		while (x < str.length()) {
-			if (str[x] <= '0' && str[x] >= '9'&& count!=(linenum || linenum-1)) break;
-			else x++;
-		}
-		if(x == str.length()-1) out << str;
-		cout << str;
-	}
-
-	in.close();
-	out.close();
+    ifstream in;
+    in.open("input.txt");
+    
+    string str;
+    int linenum = 0;
+    
+    while (getline(in, str)) {
+        cout << str<< endl;
+        linenum++;
+        
+    }
+    
+    in.close();
+    in.open("input.txt");
+    cout << "количество строк = " << linenum << endl;;
+    
+    ofstream out;
+    out.open("output.txt");
+    if(!out.is_open()){
+        cout<<"File not found";
+    }
+    else cout<<"File is opened"<<endl;
+    
+    int count = 0;
+    
+    while (getline(in, str)) {
+        cout << str << endl;
+        count++;
+        for(int i = 0; i<str.length();i++)
+            if(str[i]<='0' && str[i]>='9')
+    }
+    
+    in.close();
+    out.close();
+   
     return 0;
-}
-
