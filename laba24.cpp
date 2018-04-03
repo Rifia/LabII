a8bf3a72a221a46d6c9a8961c09db831550eeda0
Rifia <katerain45@gmail.com>
2018-04-03 09:36:00 +0300

    Update laba24.cpp

--- laba24.cpp
+++ laba24.cpp
@@ -2,18 +2,37 @@
 #include <vector>
 using namespace std;
 int main() {
-    double sum = 0.0; int s = 0, r, l, min = 0, n;
-	  cin >> n; 
-    vector<double> a; a.reserve(n); 
+    double sum = 0.0; int s = 0, r = 0, l =0, min = 0, n;
+    cin >> n;
+    vector<double> a; a.reserve(n);
     
-	for (int i = 0; i < n; i++) {
-	cin >> a[i];
-	if (a[i]<0){sum += a[i]; s++;} 
-		else{ 
-			if(sum < min){min = sum; r = i-1; l = r-s+1;} 
-			sum = 0.0; s = 0.0;
-			}
-	}
-	for (int i = l; i <= r; i++) cout << a[i] << ' ';
-	return 0;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        if (a[i] < 0){
+            sum += a[i];
+            s++;
+            }
+        
+        if (i == n-1 && sum < min){
+                r = i;
+                l = r - s + 1;
+            }
+        
+        
+        if (a[i] >= 0){
+            if(sum < min){
+                min = sum;
+                r = i - 1;
+                l = r - s + 1;
+                }
+            sum = 0.0;
+            s = 0;
+            }
+    }
+    
+    for (int i = l; i <= r; i++) cout << a[i] << ' ';
+    
+    cout << endl;
+    
+    return 0;
 }
