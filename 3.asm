812bd1eaba4a4f87425299a408da8e00ca3bf152
Rifia <katerain45@gmail.com>
2018-05-18 21:30:58 +0300

    Update 3.asm

--- 3.asm
+++ 3.asm
@@ -3,7 +3,7 @@ org 100h
 .data 
 str db 'ERROR! null a','$' 
 c dw -1 
-d dw 1 
+d dw 0 
 a db 8
 t dw -2
 q db 0 
@@ -11,27 +11,27 @@ et dw 82
 result dw 0 
 .code 
 
-mov ax,[t] ;-2*c=2 
-imul [c]     ;ax=2
+mov ax,[t] ;-2*c
+imul [c]     ;ax
 
-mov [t], ax  ;two=-2*c=2
-mov ax, 0    ;ax=0
+mov [t], ax  ;t=-2*c
+mov ax, 0    
 
-mov ax, [d]  ;d*82=0 
-imul [et]    ;ax=0
+mov ax, [d]  ;d*82 
+imul [et]    ;ax
 
-add ax, [t] ;ax=-2*c+d*82=2
+add ax, [t] ;ax=-2*c+d*82
 
-mov [c], ax   ;c=2(chislitel)    
+mov [c], ax   ;c(chislitel)    
 mov ax, 0
 
 mov cl, [a]
 cmp cl, 4
 jz exitnull
-shr cl, 2
-sub cl, 1 
+shr cl, 2   
+sub cl, 1  
 
-mov [q], cl  ;q=1(znamenatel)
+mov [q], cl  ;q(znamenatel)
 mov cx, 0      
 
 mov ax, [c]
