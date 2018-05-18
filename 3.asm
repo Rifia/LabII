a92b2ba4da728e3d3d3f1f9704eac1a3a5ea89a7
Rifia <katerain45@gmail.com>
2018-05-18 19:46:40 +0300

    Update 3.asm

--- 3.asm
+++ 3.asm
@@ -1,34 +1,43 @@
-
 .model tiny 
-    org 100h  
+org 100h 
 .data 
-    a dd 8 
-    c dd -1 
-    d dd 0
-    result dd 0
+str db 'ERROR! null a','$' 
+a dd 0 
+c dd -1 
+d dd 0 
+result dd 0 
 .code 
-    mov ax,[c]
-    mov bx, -2
-	imul bx
-	
-	xor bx, bx
-	
-	mov cx, [d]
-	mov bx,82
-	imul bx 
-	
-	xor bx,bx
-	
-	add ax, cx
-	
-	xor cx, cx
-	
-	mov cx, [a] 
-	shr cx, 2
-	dec cx
-	
-	mov bx, ax 
-	idiv cx   
-	
-	mov result, bx
-ret  
+mov ax,[c] 
+mov bx, -2 
+imul bx 
+
+xor bx, bx 
+
+mov cx, [d] 
+mov bx,82 
+imul bx 
+
+xor bx,bx 
+
+add ax, cx 
+
+xor cx, cx 
+
+mov cx, [a] 
+cmp cx,0 
+jz exitnull 
+shr cx, 2 
+dec cx 
+
+mov bx, ax 
+idiv cx 
+
+mov result, bx 
+jmp exit 
+exit: 
+ret 
+exitnull: 
+mov ah, 9 
+mov dx, offset str 
+int 21h 
+ret
